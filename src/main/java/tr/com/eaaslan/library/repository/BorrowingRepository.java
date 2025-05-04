eeb45ea8ae8100125181bc085a3e9ecf6c799530
eaaslan <ensa.aslan@gmail.com>
2025-05-04 18:04:36 +0300

    feat(borrowing-repository): add borrowing repository layer

--- src/main/java/tr/com/eaaslan/library/repository/BorrowingRepository.java
+++ src/main/java/tr/com/eaaslan/library/repository/BorrowingRepository.java
@@ -1,4 +1,8 @@
 package tr.com.eaaslan.library.repository;
 
-public interface BorrowingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import tr.com.eaaslan.library.model.Borrowing;
+
+public interface BorrowingRepository extends JpaRepository<Borrowing, Long> {
+    
 }
